774f3f9434059a23507e41505eed13f8f07c0b7d
asalisinasa <asalisinasa@gmail.com>
2018-02-12 19:05:10 +0300

    Styles and img tasks

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,3 +1,12 @@
-const img = () => console.log('Здесь я собираю оптимизирую графику');
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var imagemin = require('gulp-imagemin');
+
+const img = gulp.task('img', function() {
+    return gulp.src('source/static/img/**/*.{jpg,jpeg,png,svg}')
+        .pipe(plumber())
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
 
 export default img;
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,3 +1,19 @@
-const styles = () => console.log('Здесь я работаю со стилями');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var replace = require('gulp-replace');
+var plumber = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
+
+const styles = gulp.task('styles', function() {
+    return gulp.src('source/static/scss/entry/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(replace('url("/img', 'url("img'))
+        .pipe(replace('url(/img', 'url(img'))
+		.pipe(autoprefixer())
+		.pipe(minifyCss())
+        .pipe(gulp.dest('dist/css'));
+});
 
 export default styles;
